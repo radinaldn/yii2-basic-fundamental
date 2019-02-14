47e2e09afa959c3fce338e044368525407060c70
Radina Dwiki N <radinal.dwiki.novendra@students.uin-suska.ac.id>
2019-02-15 05:01:34 +0700

    first commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=belajar_yii2_fundamental',
+    'username' => 'root',
+    'password' => 'T1e2k3n4i5k6',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
---
+++ models/User.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace app\models;
+
+use yii\web\IdentityInterface;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id_user
+ * @property string $username
+ * @property string $password
+ * @property string $authKey
+ * @property string $accessToken
+ * @property string $role
+ */
+class User extends \yii\db\ActiveRecord implements IdentityInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'password', 'authKey', 'accessToken', 'role'], 'required'],
+            [['role'], 'string'],
+            [['username'], 'string', 'max' => 100],
+            [['password'], 'string', 'max' => 255],
+            [['authKey', 'accessToken'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id_user' => 'Id User',
+            'username' => 'Username',
+            'password' => 'Password',
+            'authKey' => 'Auth Key',
+            'accessToken' => 'Access Token',
+            'role' => 'Role',
+        ];
+    }
+
+    public static function findIdentity($id)
+    {
+        // mencari user berdasarkan ID dan yg dicari hanya 1
+        $user = User::findOne($id);
+
+        if (count($user)) {
+            return new static($user);
+        }
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        // mencari user berdasarkan accesToken dan yang dicari hanya 1
+        $user = User::find()->where(['accessToken' => $token])->one();
+
+        if (count($user)) {
+            return new static($user);
+        }
+
+        return null;
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+        // mencari user berdasarkan username dan yang dicari haya 1
+        $user = User::find()->where(['username' => $username])->one();
+
+        if (count($user)) {
+            return new static($user);
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id_user;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return $this->authKey;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->authKey === $authKey;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+}
